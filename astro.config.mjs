46d4ae812ef076669e6691ead38564643a0d0e5d
Micah Asowata <andrewgary963@gmail.com>
2024-06-26 22:01:41 +0100

    feat: add tailwind for styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import mdx from "@astrojs/mdx";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
-    image: {
-    domains: ["githubusercontent.com"],
+  integrations: [mdx(), tailwind()],
+  image: {
+    domains: ["githubusercontent.com"]
   }
-  });
+});
