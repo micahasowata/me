f39db6f63fef328028fcf4cbb8b301b24ac2f7a2
Micah Asowata <andrewgary963@gmail.com>
2024-06-30 20:37:15 +0100

    feat: made a few changes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from 'tailwindcss/defaultTheme'
 export default {
 	content: ['./src/**/*.astro'],
 	darkMode: 'class',
 	theme: {
 		extend: {
-			
+			fontFamily: {
+        		lucky: ['Luckiest Guy', ...defaultTheme.fontFamily.sans],
+				sora: ['Sora Variable', ...defaultTheme.fontFamily.sans],
+      		},
 		},
 	},
   plugins: [
