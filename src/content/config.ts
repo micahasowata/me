4cbb4b022728f8ae9587f19fd23e0fbcfee21a94
Micah Asowata <andrewgary963@gmail.com>
2024-08-19 18:28:34 +0100

    feat: reduce post schema
    the previous schema had a lot of fields that over time became extraneous
    so I removed them

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,19 +1,14 @@
 import { defineCollection, z } from "astro:content";
 
 const posts = defineCollection({
-  type: "content",
-  schema: z.object({
-    title: z.string(),
-    pubDate: z.date(),
-    intro: z.string(),
-    tags: z.array(z.string()),
-    cover: z.object({
-      src: z.string(),
-      alt: z.string(),
-    }),
-  }),
+	type: "content",
+	schema: z.object({
+		title: z.string(),
+		published: z.date(),
+		summary: z.string(),
+	}),
 });
 
 export const collections = {
-  posts,
+	posts,
 };
