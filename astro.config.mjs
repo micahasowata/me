13c5f79256c1d5cdee677e193ff2d143e8629404
Micah Asowata <andrewgary963@gmail.com>
2024-08-19 18:57:51 +0100

    fix: provide uniform og image for website

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,13 @@
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [mdx(), tailwind()],
   image: {
-    domains: ["githubusercontent.com", "utfs.io"]
+    domains: ["utfs.io"],
   },
   build: {
-    format: "file"
-  }
+    format: "file",
+  },
 });
