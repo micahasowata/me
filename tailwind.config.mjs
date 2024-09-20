da003d621b0107bb631a2619eb1c4cc091f538ff
Micah Asowata <andrewgary963@gmail.com>
2024-09-20 18:26:47 +0100

    feat: update blog font
    - add fraunces

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,16 +1,15 @@
 /** @type {import('tailwindcss').Config} */
-import defaultTheme from 'tailwindcss/defaultTheme'
+import defaultTheme from "tailwindcss/defaultTheme";
 export default {
-	content: ['./src/**/*.astro'],
-	darkMode: 'class',
-	theme: {
-		extend: {
-			fontFamily: {
-				sora: ['Sora Variable', ...defaultTheme.fontFamily.sans],
-      		},
-		},
-	},
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
-}
+  content: ["./src/**/*.astro"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      fontFamily: {
+        sora: ["Sora Variable", ...defaultTheme.fontFamily.sans],
+        fraunces: ["Fraunces Variable", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
